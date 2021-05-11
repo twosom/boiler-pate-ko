8d1001d3bfb2f281186f010cb477899c859ceb21
desirel <two_somang@icloud.com>
2021-05-11 22:48:45 +0900

    수정

--- index.js
+++ index.js
@@ -2,12 +2,25 @@ const express = require('express');
 const app = express();
 const port = 5000;
 
+const {User} = require('./models/User');
+const bodyParser = require('body-parser');
+
+const config = require('./config/key');
+
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+//application/json
+app.use(bodyParser.json());
+
 const mongoose = require('mongoose');
 
 /**
  * MongoDB 연결
  */
-mongoose.connect('mongodb+srv://twosom:twosom123@projecttwosom.livyn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+mongoose.connect(config.mongoURI, {
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(() => {
     console.log(`MongoDB Connected...`);
@@ -17,9 +30,27 @@ mongoose.connect('mongodb+srv://twosom:twosom123@projecttwosom.livyn.mongodb.net
 
 
 app.get('/', (request, response) => {
-    response.send('Hello World');
+    response.send('My name is twosom');
 });
+
+
+app.post('/register', (request, response) => {
+    const user = new User(request.body);
+
+    user.save((error, userInfo) => {
+        // 실패 시
+        if (error) return response.json({
+            success: false,
+            error
+        });
+        // 성공 시
+        return response.status(200).json({
+            success: true
+        });
+    });
+});
+
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}!`);
-})
+});
